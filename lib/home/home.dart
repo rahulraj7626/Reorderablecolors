cee44c61ba29bcca8745f0a4720e9f77e556e0bc
rahulraj7626 <rahulraj7626@gmail.com>
2022-01-15 01:10:15 +0530

    fimish

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -13,15 +13,92 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List data = [
-    {'image': 'assets/black1.png', 'name': 'BLACK', 'pts': '17'},
-    {'image': 'assets/blue1.png', 'name': 'BLUE', 'pts': '59'},
-    {'image': 'assets/green1.png', 'name': 'GREEN', 'pts': '43'},
-    {'image': 'assets/orange1.png', 'name': 'ORANGE', 'pts': '29'},
-    {'image': 'assets/pink1.png', 'name': 'PINK', 'pts': '9'},
-    {'image': 'assets/purple1.png', 'name': 'PURPLE', 'pts': '17'},
-    {'image': 'assets/red1.png', 'name': 'RED', 'pts': '78'},
-    {'image': 'assets/teal1.png', 'name': 'TEAL', 'pts': '14'},
-    {'image': 'assets/yellow1.png', 'name': 'YELLOW', 'pts': '66'}
+    {
+      'image': 'assets/black1.png',
+      'name': 'BLACK',
+      'pts': '17',
+      'img_list': [
+        {'path': 'assets/black1.png'}
+      ]
+    },
+    {
+      'image': 'assets/blue1.png',
+      'name': 'BLUE',
+      'pts': '59',
+      'img_list': [
+        {'path': 'assets/blue1.png'},
+        {'path': 'assets/blue2.png'},
+        {'path': 'assets/blue3.png'}
+      ]
+    },
+    {
+      'image': 'assets/green1.png',
+      'name': 'GREEN',
+      'pts': '43',
+      'img_list': [
+        {'path': 'assets/green1.png'},
+        {'path': 'assets/green2.png'},
+        {'path': 'assets/green3.png'}
+      ]
+    },
+    {
+      'image': 'assets/orange1.png',
+      'name': 'ORANGE',
+      'pts': '29',
+      'img_list': [
+        {'path': 'assets/orange1.png'},
+        {'path': 'assets/orange2.png'},
+        {'path': 'assets/orange3.png'}
+      ]
+    },
+    {
+      'image': 'assets/pink1.png',
+      'name': 'PINK',
+      'pts': '9',
+      'img_list': [
+        {'path': 'assets/pink1.png'},
+        {'path': 'assets/pink2.png'},
+        {'path': 'assets/pink3.png'}
+      ]
+    },
+    {
+      'image': 'assets/purple1.png',
+      'name': 'PURPLE',
+      'pts': '17',
+      'img_list': [
+        {'path': 'assets/purple1.png'},
+        {'path': 'assets/purple2.png'},
+        {'path': 'assets/purple3.png'}
+      ]
+    },
+    {
+      'image': 'assets/red1.png',
+      'name': 'RED',
+      'pts': '78',
+      'img_list': [
+        {'path': 'assets/red1.png'},
+        {'path': 'assets/red2.png'},
+        {'path': 'assets/red3.png'}
+      ]
+    },
+    {
+      'image': 'assets/teal1.png',
+      'name': 'TEAL',
+      'pts': '14',
+      'img_list': [
+        {'path': 'assets/teal1.png'},
+        {'path': 'assets/teal2.png'},
+        {'path': 'assets/teal3.png'}
+      ]
+    },
+    {
+      'image': 'assets/yellow1.png',
+      'name': 'YELLOW',
+      'pts': '66',
+      'img_list': [
+        {'path': 'assets/yellow1.png'},
+      ]
+    }
   ];
 
   @override
@@ -42,7 +119,9 @@ class _HomeState extends State<Home> {
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
-                    children: data.map((e) => buildItem(e['image'])).toList(),
+                    children: data
+                        .map((e) => buildItem(e['image'], e['img_list']))
+                        .toList(),
                     onReorder: (oldIndex, newIndex) {
                       setState(() {
                         final element = data.removeAt(oldIndex);
@@ -75,18 +154,31 @@ class _HomeState extends State<Home> {
   }
 
 //return builder item widget
-  Widget buildItem(String text) {
+  Widget buildItem(String text, List path) {
     return Card(
       key: ValueKey(text),
       child: GestureDetector(
         onTap: () {
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) => ImagePreview(text)));
+              MaterialPageRoute(builder: (context) => ImagePreview(path)));
         },
-        child: Container(
-          decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage(text), fit: BoxFit.cover)),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(text), fit: BoxFit.cover)),
+            ),
+            path.length == 1
+                ? const SizedBox()
+                : Positioned(
+                    top: 2,
+                    right: 2,
+                    child: GestureDetector(
+                        onTap: () {},
+                        child: Image.asset('assets/icon_pictures.png')),
+                  ),
+          ],
         ),
       ),
     );
--- lib/widgets/image_view.dart
+++ lib/widgets/image_view.dart
@@ -2,31 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
 class ImagePreview extends StatelessWidget {
-//  final List images; //for list of images
-  final String image;
-  const ImagePreview(this.image, {Key? key}) : super(key: key);
+  final List images; //for list of images
+  // final String image;
+  const ImagePreview(this.images, {Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PhotoViewGallery.builder(
-        itemCount: 1, //1. for single image , images.length for mutiple
-        builder: (context, index) {
-          return PhotoViewGalleryPageOptions(
-            imageProvider: AssetImage(image),
-          );
-        },
-        scrollPhysics: const BouncingScrollPhysics(),
-        backgroundDecoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-        ),
-        loadingBuilder: (context, event) => const Center(
-          child: SizedBox(
-            width: 30.0,
-            height: 30.0,
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.white,
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            PhotoViewGallery.builder(
+              itemCount: images
+                  .length, //1. for single image , images.length for mutiple
+              builder: (context, index) {
+                return PhotoViewGalleryPageOptions(
+                  imageProvider: AssetImage(images[index]['path']),
+                );
+              },
+              scrollPhysics: const BouncingScrollPhysics(),
+              backgroundDecoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+              ),
+              loadingBuilder: (context, event) => const Center(
+                child: SizedBox(
+                  width: 30.0,
+                  height: 30.0,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              top: 20,
+              right: 20,
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 2),
+                          borderRadius: BorderRadius.circular(40)),
+                      child: const Icon(Icons.close))),
+            ),
+          ],
         ),
       ),
     );
